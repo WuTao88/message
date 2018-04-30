6ef0679019c3b9ad298c38389d7614e2ef5b3c1c
wutao <wutao.88@qq.com>
2018-04-30 14:10:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,20 +5,4 @@
  * Date: 2017/12/28
  * Time: 12:41
  */
-date_default_timezone_set('PRC');
-$date=date("Ymd");
-
-$dir=dir("msg");
-
-$file;
-while (($file=$dir->read())!== false){
-    if($file!="."&&$file!="..") {
-        $files = fopen("msg/". $file, "r") or exit("无法打开文件!");
-
-        while (!feof($files)) {
-            echo fgets($files) . "<br>";
-        }
-        fclose($files);
-    }
-}
-$dir->close();
+echo "<h4>Hello world</h4>";
