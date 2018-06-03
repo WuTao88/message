84a18d0e7820a15171022130f6664453a72289ed
wutao <wutao.88@qq.com>
2018-06-03 13:54:06 +0800

    Update table.php

--- table.php
+++ table.php
@@ -1,8 +1,8 @@
 <?php
-$user = 'root';      //数据库连接用户名
+$user = 'hook';      //数据库连接用户名
 $pass = '123456';    //对应的密码
-$dbname='test';      //数据库名字
-$conn = new PDO("mysql:host=localhost;dbname=$dbname", $user, $pass)or
+$dbname='exampleDB';      //数据库名字
+$conn = new PDO("mysql:host=172.30.202.100;dbname=$dbname", $user, $pass)or
 
 die("***数据访问出错***");
 
