dfed0988a19405a46991076f1d3d2a2f5fd188cb
wutao <wutao.88@qq.com>
2018-06-03 13:45:26 +0800

    Create table.php

---
+++ table.php
@@ -0,0 +1,17 @@
+<?php
+$user = 'root';      //数据库连接用户名
+$pass = '123456';    //对应的密码
+$dbname='test';      //数据库名字
+$conn = new PDO("mysql:host=localhost;dbname=$dbname", $user, $pass)or
+
+die("***数据访问出错***");
+
+echo "✔";
+
+$sql=file_get_contents("test.sql");
+
+$result =$conn->query($sql);
+
+if($result>0){
+echo "success";
+}
